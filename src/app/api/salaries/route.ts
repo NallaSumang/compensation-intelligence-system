b33c6ef2c2f8b75416f9b1ee7566d7be11e81dd5
NALLA SUMANG <sumangsumang41@gmail.com>
2026-05-06 18:24:06 +0530

    Update route.ts

--- src/app/api/salaries/route.ts
+++ src/app/api/salaries/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import  prisma  from "@/lib/prisma";
 import { z } from "zod";
 
 // 1. Define strict validation for incoming query parameters
